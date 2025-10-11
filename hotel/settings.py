772f95fdfd5ee2ba4ef193f9ea3111b254079361
ipiteuber <140553021+ipiteuber@users.noreply.github.com>
2025-10-11 16:29:54 -0300

    Actualizacion settings

--- hotel/settings.py
+++ hotel/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-92q+81n-tpbicwbmr-un%3uz+nr-+-s7&$9g-q$lx&q0d9%dq5'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'dev-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
